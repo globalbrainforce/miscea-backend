20bbda4e76c4d01fbd80b219ae3c87b241e5dddf
Krisfen G. Ducao <krisfen.ducao@globalbrainforce.com>
2020-12-09 02:31:23 +0000

    update code

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+# pylint: disable=wrong-import-position, import-error, unused-import
+""" APP """
 from flask import request
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
@@ -11,32 +13,15 @@ CORS(APP)
 
 SOCKETIO = SocketIO(APP, cors_allowed_origins="*")
 
-@SOCKETIO.on('connect')
-def test_connect():
-    """ CONNECT """
-    print("ARGS: ", request.args)
-    print("ARGS: ", request.sid)
-    emit('my response', {'data': 'Connected'})
+# CONNECTION EVENTS
+from events.connection import connection
 
-@SOCKETIO.on('disconnect')
-def test_disconnect():
-    """ DISCONNECT """
-    print('Client disconnected')
-    print("ARGS: ", request.sid)
+# AUTH EVENTS
+from events.auth import auth
 
-def message_recived():
-    """ RECEIVED MESSAGE """
-    print('message was received!!!')
+# MESSAGE EVENTS
+from events.message import message
 
-@SOCKETIO.on('token')
-def handle_my_custom_event(json):
-    """ HANDLE MESSAGE """
-    clients = []
-    clients.append(request.sid)
-    print('recived my event: ' + str(json))
-    json3 = {'user_name': "Bot", 'message': "Hello!"}
-    SOCKETIO.emit('my response', json, callback=message_recived, room=clients[0])
-    SOCKETIO.emit('my response', json3, callback=message_recived, room=clients[0])
 
 @APP.route('/')
 def home():
