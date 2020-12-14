aa03792f0bde9d735cd50a4acd2b802fe251fe01
Krisfen G. Ducao <krisfen.ducao@globalbrainforce.com>
2020-12-14 22:52:30 +0000

    update code

--- events/connection/connection.py
+++ events/connection/connection.py
@@ -16,12 +16,16 @@ except ImportError:
 @SOCKETIO.on('connect')
 def connect():
     """ CONNECT """
+    print("*"*100)
     print("ARGS: ", request.args)
     print("ARGS: ", request.sid)
     emit('my response', {'data': 'Connected'})
+    print("*"*100)
 
 @SOCKETIO.on('disconnect')
 def disconnect():
     """ DISCONNECT """
+    print("*"*100)
     print('Client disconnected')
     print("ARGS: ", request.sid)
+    print("*"*100)
