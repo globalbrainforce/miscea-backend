cb4dbfa7bf271a594c9e1b176f0f55c05e2a9c5d
Penn Ducao <penn.ducao@gmail.com>
2020-12-14 01:52:14 +0000

    update code

--- README.md
+++ README.md
@@ -3,5 +3,8 @@
 ```bash
 sudo pip3 freeze > requirements.txt
 
+
+sudo apt-get install gunicorn
 sudo pip3 install -r requirements.txt
+
 ```
--- app.py
+++ app.py
@@ -8,10 +8,10 @@ from flask_cors import CORS
 # https://flask-socketio.readthedocs.io/en/latest/
 # https://github.com/socketio/socket.io-client
 
-APP = Flask(__name__, template_folder='ui')
-CORS(APP)
+application = Flask(__name__, template_folder='ui')
+CORS(application)
 
-SOCKETIO = SocketIO(APP, cors_allowed_origins="*")
+SOCKETIO = SocketIO(application, cors_allowed_origins="*")
 
 # CONNECTION EVENTS
 from events.connection import connection
@@ -23,7 +23,7 @@ from events.auth import auth
 from events.message import message
 
 
-@APP.route('/')
+@application.route('/')
 def home():
     """ HTML """
     return render_template('chat_ui.html')
