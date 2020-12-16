384cd3c6a5be438bfd77d9171ae7de4ec132f6ba
Krisfen G. Ducao <krisfen.ducao@globalbrainforce.com>
2020-12-16 00:27:43 +0000

    update code

--- events/message/message.py
+++ events/message/message.py
@@ -28,7 +28,7 @@ def message(json):
 
     if json['token'] == '300c2c3706886d94aeefd6e7f7130ab08346590533d4c5b24ccaea9baa5211ed':
 
-        response = {'user_name': "Bot", 'message': "Message received: " + str(json['message'])}
+        response = {'top_id': "Bot", 'message': "Message received: " + str(json['message'])}
         emit('my response', json, room=clients[0])
         emit('my response', response, room=clients[0])
 
