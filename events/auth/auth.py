8e86ae06b8dfcdf600f21124d9c5c618b007c780
Krisfen G. Ducao <krisfen.ducao@globalbrainforce.com>
2021-01-05 23:10:06 +0000

    update code

--- events/auth/auth.py
+++ events/auth/auth.py
@@ -1,5 +1,6 @@
 """ AUTH """
 import json
+import time
 import asyncio
 
 async def auth(websocket, data):
@@ -19,6 +20,7 @@ async def auth(websocket, data):
 
         message = {}
         message['type'] = 'auth'
+        message['time'] = time.time()
         message['status'] = 'ok'
         message['new_token'] = '300c2c3706886d94aeefd6e7f7130ab08346590533d4c5b24ccaea9baa5211ed'
         message = json.dumps(message)
