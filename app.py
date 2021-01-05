3cd10e6ebc575d9ea3bac829ddf77c026b27b7b5
Krisfen G. Ducao <krisfen.ducao@globalbrainforce.com>
2021-01-05 00:16:41 +0000

    updated code

--- app.py
+++ app.py
@@ -9,64 +9,26 @@ from events.auth import auth
 
 logging.basicConfig()
 
-STATE = {"value": 0}
-
 USERS = set()
 
-
-# def state_event():
-#     return json.dumps({"type": "state", **STATE})
-
-
-# def users_event():
-#     return json.dumps({"type": "users", "count": len(USERS)})
-
-
-# async def notify_state():
-#     if USERS:  # asyncio.wait doesn't accept an empty list
-#         message = state_event()
-#         await asyncio.wait([user.send(message) for user in USERS])
-
-
-# async def notify_users():
-#     if USERS:  # asyncio.wait doesn't accept an empty list
-#         message = users_event()
-#         await asyncio.wait([user.send(message) for user in USERS])
-
-
-async def register(websocket):
-    USERS.add(websocket)
-    # await notify_users()
-
-
-async def unregister(websocket):
-    USERS.remove(websocket)
-    # await notify_users()
-
 async def app(websocket, path):
+    """ MAIN APPLICATION """
 
-    # register(websocket)
     USERS.add(websocket)
-    try:
-        # await websocket.send(state_event())
-        async for message in websocket:
-            print("message here!")
-            data = json.loads(message)
-            if path == '/auth':
-                print("AUTH Called!")
-                await auth.auth(USERS, data)
-            # if data["action"] == "minus":
-            #     STATE["value"] -= 1
-            #     await notify_state()
-            # elif data["action"] == "plus":
-            #     STATE["value"] += 1
-            #     await notify_state()
-            # else:
-            #     logging.error("unsupported event: {}", data)
-    finally:
-        USERS.remove(websocket)
-        # unregister(websocket)
 
+    try:
+
+        async for message in websocket:
+
+            data = json.loads(message)
+
+            if path == '/auth':
+
+                await auth.auth(USERS, data)
+
+    finally:
+
+        USERS.remove(websocket)
 
 MAIN = websockets.serve(app, "0.0.0.0", 6789)
 
