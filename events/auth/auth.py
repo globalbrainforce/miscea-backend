26a2038915c6e805e78c7268fa54a7c3804a252d
Krisfen G. Ducao <krisfen.ducao@globalbrainforce.com>
2021-01-06 01:50:44 +0000

    update code

--- events/auth/auth.py
+++ events/auth/auth.py
@@ -24,47 +24,52 @@ async def auth(websocket, data):
 
     if data['token'] == '269c2c3706886d94aeefd6e7f7130ab08346590533d4c5b24ccaea9baa5211ec':
 
-        system_id = 'system:' + '43876adc83a24d6191c2edd4158de8740'
+        default = data['system_data']
+        system_id = default['system_id']
+        establishment = 'establishment:381741ac4b5f4a4785ffdf2e025975fc'
+
         doc = COUCH_QUERY.get_by_id(system_id)
-        print("system: ", doc)
-        establishment = "establishment:381741ac4b5f4a4785ffdf2e025975fc"
+
         # CHECK IF SYSTEM ID EXIST
         # IF NOT
+        if 'error' in doc.keys():
             # ADD AS NEW TOP
-            # RETURN SYSTEM ID
+
+            current = time.time()
+            system = {}
+            system['_id'] = system_id
+            system['article_number'] = default['article_number']
+            system['update_on'] = current
+            system['created_on'] = current
+            system['serial'] = str(SHA_SECURITY.generate_token(False))[:10]
+            system['model'] = ""
+            system['description'] = ""
+            system['soap_dose'] = default['soap_dose']
+            system['disinfect_dose'] = default['disinfect_dose']
+            system['init_wtr_temp'] = default['init_wtr_temp']
+            system['wtr_shut_off_dly'] = default['wtr_shut_off_dly']
+            system['wtr_temp_mem_tm'] = default['wtr_temp_mem_tm']
+            system['bucket_mode_d'] = default['bucket_mode_d']
+            system['tm_b4_stagn_flsh'] = default['tm_b4_stagn_flsh']
+            system['stagn_flsh_d'] = default['stagn_flsh_d']
+            system['stagn_flsh_u_dep'] = default['stagn_flsh_u_dep']
+            system['thrm_flshng_tm'] = default['thrm_flshng_tm']
+            system['thrm_flshng_day'] = default['thrm_flshng_day']
+            system['thrm_flsh_temp'] = default['thrm_flsh_temp']
+            system['thrm_flsh_d'] = default['thrm_flsh_d']
+            system['light_effect'] = default['light_effect']
+            system['beep_tone'] = default['beep_tone']
+            system['clean_mode'] = default['clean_mode']
+            system['flow_heater_mode'] = default['flow_heater_mode']
+            system['ir_range'] = default['ir_range']
+            system['type'] = "systems_list"
+            system['establishment_id'] = establishment
+
         # ELSE
+        else:
+
+            pass
             # RETURN SYSTEM ID
-
-        # current = time.time()
-        # system = {}
-        # system['_id'] = system_id
-        # system['article_number'] = 'CER2S1NCDE'
-        # system['update_on'] = current
-        # system['created_on'] = current
-        # system['serial'] = str(SHA_SECURITY.generate_token(False))[:10]
-        # system['model'] = ""
-        # system['description'] = ""
-        # system['soap_dose'] = default['soap_dose']
-        # system['disinfect_dose'] = default['disinfect_dose']
-        # system['init_wtr_temp'] = default['init_wtr_temp']
-        # system['wtr_shut_off_dly'] = default['wtr_shut_off_dly']
-        # system['wtr_temp_mem_tm'] = default['wtr_temp_mem_tm']
-        # system['bucket_mode_d'] = default['bucket_mode_d']
-        # system['tm_b4_stagn_flsh'] = default['tm_b4_stagn_flsh']
-        # system['stagn_flsh_d'] = default['stagn_flsh_d']
-        # system['stagn_flsh_u_dep'] = default['stagn_flsh_u_dep']
-        # system['thrm_flshng_tm'] = default['thrm_flshng_tm']
-        # system['thrm_flshng_day'] = default['thrm_flshng_day']
-        # system['thrm_flsh_temp'] = default['thrm_flsh_temp']
-        # system['thrm_flsh_d'] = default['thrm_flsh_d']
-        # system['light_effect'] = default['light_effect']
-        # system['beep_tone'] = default['beep_tone']
-        # system['clean_mode'] = default['clean_mode']
-        # system['flow_heater_mode'] = default['flow_heater_mode']
-        # system['ir_range'] = default['ir_range']
-        # system['type'] = "systems_list"
-        # system['establishment_id'] = establishment
-
 
         message = {}
         message['type'] = 'auth'
