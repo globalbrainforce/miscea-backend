14cadaa626844308de657feb983c9b2d0bedead9
Krisfen G. Ducao <krisfen.ducao@globalbrainforce.com>
2021-01-21 01:05:07 +0000

    updated code

--- events/update_settings/update_settings.py
+++ events/update_settings/update_settings.py
@@ -4,102 +4,70 @@ import time
 import asyncio
 import syslog
 from library.common import Common
+from library.couch_queries import Queries
 from library.sha_security import ShaSecurity
 from library.postgresql_queries import PostgreSQL
 
 COMMON = Common()
+COUCH_QUERY = Queries()
 SHASECURITY = ShaSecurity()
 POSTGRES = PostgreSQL()
 
 async def update_settings(websocket, data, users):
 
-    # if not 'token' in data.keys():
+    # CHECK TOKEN EXIST
+    if not 'token' in data.keys() or not 'system_id' in data.keys():
 
-    #     syslog.syslog("NO TOKEN!")
-    #     message = {}
-    #     message['type'] = 'auth'
-    #     message['status'] = 'Failed'
-    #     message['alert'] = 'Invalid data!'
-    #     message = json.dumps(message)
-    #     await asyncio.wait([websocket.send(message)])
+        syslog.syslog("NO TOKEN!")
+        message = {}
+        message['type'] = 'auth'
+        message['status'] = 'Failed'
+        message['alert'] = 'Invalid data!'
+        message = json.dumps(message)
+        await asyncio.wait([websocket.send(message)])
 
-    #     return 0
+        return 0
 
     # VALIDATE TOKEN
+    sql_str = "SELECT token_id FROM default_tokens WHERE"
+    sql_str += " token='{0}'".format(data['token'])
+
+    if not POSTGRES.query_fetch_one(sql_str):
+
+        syslog.syslog("NO TOKEN!")
+        message = {}
+        message['type'] = 'auth'
+        message['status'] = 'Failed'
+        message['alert'] = 'Invalid data!'
+        message = json.dumps(message)
+        await asyncio.wait([websocket.send(message)])
+
+        return 0
+
     # CHECK IF TAP IS ONLINE
-    # SEND UPDATE TO THE TAP
+    system_id = data['system_id']
+
+    for item in users.items():
+
+        if not item[0] == system_id:
+
+            # SEND UPDATE TO THE TAP
+            system_info = COUCH_QUERY.get_by_id(system_id)
+            system_info['type'] = 'message'
+            system_info['system_id'] = system_id
+            system_info['msg_id'] = time.time()
+            system_info['status'] = 'update'
+            system_info = json.dumps(system_info)
+            await asyncio.wait([item[1].send(system_info)])
+
+            return 1
 
     syslog.syslog("== UPDATE ==")
     message = {}
-    message['type'] = 'auth'
+    message['type'] = 'updated'
     message['time'] = time.time()
     message['status'] = 'ok'
-    # message['new_token'] = new_token
     message = json.dumps(message)
     await asyncio.wait([websocket.send(message)])
 
     return 1
-
-    # if COMMON.validate_default_token(data['token']):
-
-    #     # default = data['system_data']
-    #     default = data
-    #     system_id = default['system_id']
-    #     new_token = SHASECURITY.generate_token(False)
-
-    #     tap_account = {}
-    #     tap_account['system_id'] = system_id
-    #     tap_account['token'] = new_token
-    #     tap_account['last_login'] = time.time()
-
-    #     sql_str = "SELECT tap_account_id FROM tap_accounts WHERE"
-    #     sql_str += " system_id='{0}'".format(system_id)
-    #     response = POSTGRES.query_fetch_one(sql_str)
-
-    #     if response:
-
-    #         # INIT CONDITION
-    #         conditions = []
-
-    #         # CONDITION FOR QUERY
-    #         conditions.append({
-    #             "col": "tap_account_id",
-    #             "con": "=",
-    #             "val": response['tap_account_id']
-    #             })
-
-    #         # UPDATE TAP ACCOUNT TOKEN
-    #         tap_account['update_on'] = time.time()
-
-    #         POSTGRES.update('tap_accounts', tap_account, conditions)
-
-    #     else:
-
-    #         # INSERT TAP ACCOUNT TOKEN
-    #         tap_account['tap_account_id'] = SHASECURITY.generate_token(False)
-    #         tap_account['created_on'] = time.time()
-
-    #         POSTGRES.insert('tap_accounts', tap_account)
-
-    #     syslog.syslog("VALID TOKEN!")
-    #     message = {}
-    #     message['type'] = 'auth'
-    #     message['time'] = time.time()
-    #     message['status'] = 'ok'
-    #     message['new_token'] = new_token
-    #     message = json.dumps(message)
-    #     await asyncio.wait([websocket.send(message)])
-
-    #     return 1
-
-    # else:
-
-    #     syslog.syslog("INVALID TOKEN!")
-    #     message = {}
-    #     message['type'] = 'auth'
-    #     message['status'] = 'Failed'
-    #     message['alert'] = 'Invalid data!'
-    #     message = json.dumps(message)
-    #     await asyncio.wait([websocket.send(message)])
-
-    #     return 0
