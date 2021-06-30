25be367b44568d18954e8a5f3f6bb05150f7f070
Krisfen G. Ducao <krisfen.ducao@globalbrainforce.com>
2021-06-30 06:10:55 +0000

    modified:   app.py

--- app.py
+++ app.py
@@ -241,7 +241,7 @@ context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
 context.load_cert_chain('/home/admin/cert/miscea.com.crt', '/home/admin/cert/miscea.key')
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0) as sock:
-    sock.bind(('127.0.0.1', 8443))
+    sock.bind(("0.0.0.0", 8443))
     sock.listen(5)
     with context.wrap_socket(sock, server_side=True) as ssock:
         conn, addr = ssock.accept()
